4f9ddd0d62565d1833b0b396d1982e2253fee505
b.amrahov <b.amrahov@xalqsigorta.az>
2023-02-03 16:30:45 +0400

    CreateUpdateCatchForDatabaseWithTracking

--- Presentation/E-Commerce.Api/Controllers/ProductController.cs
+++ Presentation/E-Commerce.Api/Controllers/ProductController.cs
@@ -1,3 +1,5 @@
+using E_Commerce.Application.Repositories.CustomerRepository;
+using E_Commerce.Application.Repositories.OrderRepository;
 using E_Commerce.Application.Repositories.ProductRepository;
 using E_Commerce.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -11,35 +13,31 @@ public class ProductController : Controller
     private readonly IProductReadRepository _productReadRepository;
     private readonly IProductWriteRepository _productWriteRepository;
 
-    public ProductController(IProductReadRepository productReadRepository, 
-                             IProductWriteRepository productWriteRepository)
+    private readonly IOrderReadRepository _orderReadRepository;
+    private readonly IOrderWriteRepository _orderWriteRepository;
+
+    private readonly ICustomerReadRepository _customerReadRepository;
+    private readonly ICustomerWriteRepository _customerWriteRepository;
+
+    public ProductController(IProductReadRepository productReadRepository,
+                             IProductWriteRepository productWriteRepository,
+                             IOrderReadRepository orderReadRepository,
+                             IOrderWriteRepository orderWriteRepository,
+                             ICustomerWriteRepository customerWriteRepository,
+                             ICustomerReadRepository customerReadRepository)
     {
         _productReadRepository = productReadRepository;
         _productWriteRepository = productWriteRepository;
+        _orderReadRepository = orderReadRepository;
+        _orderWriteRepository = orderWriteRepository;
+        _customerWriteRepository = customerWriteRepository;
+        _customerReadRepository = customerReadRepository;
     }
     // GET
     [HttpGet]
-    public async Task Index()
+    public async Task Gwt()
     {
-        List<Product> products = new List<Product>()
-        {
-            new Product()
-            {
-                Id = Guid.NewGuid(),
-                Name = "Product",
-                Stock = 100,
-                Price = 12,
-            },
-            new Product()
-            {
-                Id = Guid.NewGuid(),
-                Name = "Product2",
-                Stock = 100,
-                Price = 12,
-            }
-        };
-       await _productWriteRepository.AddRangeAsync(products);
-       await _productWriteRepository.SaveAsync();
+
     }
 
     [HttpGet("id")]
